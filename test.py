656b55154a0fec7239f99a606c56bfa9124fdced
Giova <giovanni.rocci@uzh.ch>
2025-04-30 16:55:06 +0200

    test context version

--- test.py
+++ test.py
@@ -1,20 +1,30 @@
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer
-
-model = AutoModelForSequenceClassification.from_pretrained("distilbert-tox-detection/best_model")
-tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
-
-# If needed, reload test dataset
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 from datasets import Dataset
 import pandas as pd
+import argparse
 
-test_ds = Dataset.from_pandas(pd.read_csv("data/test.csv").dropna())
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_name", "-m", type=str, required=True)
+parser.add_argument("--tokenizer", "-t", type=str, default="distilbert-base-uncased")
+parser.add_argument("--test_dataset", "-td", type=str, required=True)
+parser.add_argument("--output_file", "-o", type=str)
+args = parser.parse_args()
+
+model = AutoModelForSequenceClassification.from_pretrained(args.model_name)
+tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
+
+
+test_ds = Dataset.from_pandas(pd.read_csv(args.test_dataset).dropna())
 test_ds = test_ds.map(lambda x: tokenizer(x["text"], padding=True, truncation=True, max_length=512), batched=True)
 test_ds = test_ds.map(lambda x: {"label": {"E":0,"I":1,"A":2,"O":3}[x["labels"]]})
-test_ds = test_ds.remove_columns("labels")
-test_ds.set_format(type="torch", columns=["input_ids", "attention_mask", "label"])
 
-# Then reuse Trainer
-trainer = Trainer(model=model)
+pipe = pipeline("text-classification", model=model, tokenizer=tokenizer)
 
-test_results = trainer.predict(test_ds)
-print(test_results.metrics)
+for utterance in test_ds:
+    print(utterance)
+    result = pipe(utterance["text"])
+    print(result)
+    if args.output_file:
+        with open(args.output_file, "a") as f:
+            f.write(f"{utterance['text']},{result[0]['label']}\n")
